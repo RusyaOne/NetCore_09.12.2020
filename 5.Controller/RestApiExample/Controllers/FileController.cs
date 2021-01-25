8785c5671c2316dfd9e719885d5c68110195e519
Ruslan Antoshkin <ruslan.antoshkin@zerto.com>
2021-01-25 14:51:48 +0200

    Updated gitignore

---
+++ 5.Controller/RestApiExample/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using BasicInfo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicInfo.Controllers
+{
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        //private INewsRepository _newsRepository { get; }
+
+        //public FileController(INewsRepository newsRepository)
+        //{
+        //    _newsRepository = newsRepository;
+        //}
+
+        [HttpGet("File")]
+        public FileContentResult GetFile()
+        {
+
+        }
+    }
+}
