87b9e4966352d3d8ca57332102ba7af8811d6def
Ruslan Antoshkin <ruslan.antoshkin@zerto.com>
2021-01-25 18:41:20 +0200

    Added websockets example

---
+++ 11.WebSockets/WebSocketsExample/wwwroot/js/chatstream.js
@@ -0,0 +1,23 @@
+﻿function OpenConnection() {
+    let webSocket
+    var getWebSocketMessages = function (onMessageReceived) {
+        let username = document.getElementById('username');
+        let url = `wss://${location.host}/stream/get?username=${username.value}`;
+        webSocket = new WebSocket(url);
+
+        webSocket.onmessage = onMessageReceived;
+    };
+
+    let ulElement = document.getElementById('chatMessages');
+
+    getWebSocketMessages(function (message) {
+        ulElement.innerHTML = ulElement.innerHTML += `<li>${message.data}</li>`
+    });
+
+    document.getElementById("sendmessage").addEventListener("click", function () {
+        let textElement = document.getElementById("messageTextInput");
+        let text = textElement.value;
+        webSocket.send(text);
+        textElement.value = '';
+    });
+}
